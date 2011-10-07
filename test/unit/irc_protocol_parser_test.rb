f2e6e04d413d02f803baf93204f00e98bc93669d
Cainã Costa <cainan.costa@gmail.com>
2011-10-06 23:29:56 -0300

    Change the test variable name to a more correct one.

--- test/unit/irc_protocol_parser_test.rb
+++ test/unit/irc_protocol_parser_test.rb
@@ -3,50 +3,50 @@ require 'stewie/irc_protocol_parser.rb'
 
 class IrcProtocolParserTest < MiniTest::Unit::TestCase
   def setup
-    @protocol = Stewie::IrcProtocolParser.new
+    @parser = Stewie::IrcProtocolParser.new
   end
 
   def test_parse_user_connection
     message = ':ronnie USER guest tolmoon toolsun :Ronnie Reagan'
-    assert_equal [:user, 'ronnie', 'guest', 'tolmoon', 'toolsun', 'Ronnie Reagan'], @protocol.parse(message)
+    assert_equal [:user, 'ronnie', 'guest', 'tolmoon', 'toolsun', 'Ronnie Reagan'], @parser.parse(message)
   end
 
   def test_parse_ping
     message = 'PING :WiZ'
-    assert_equal [:ping, 'WiZ'], @protocol.parse(message)
+    assert_equal [:ping, 'WiZ'], @parser.parse(message)
   end
 
   def test_parse_channel_join
     message = ':WiZ JOIN #Twilight_zone'
-    assert_equal [:join, 'WiZ', '#Twilight_zone'], @protocol.parse(message)
+    assert_equal [:join, 'WiZ', '#Twilight_zone'], @parser.parse(message)
   end
 
   def test_parse_channel_nick_change
-    assert_equal [:nick, 'WiZ', 'Kilroy'], @protocol.parse(':WiZ NICK Kilroy')
+    assert_equal [:nick, 'WiZ', 'Kilroy'], @parser.parse(':WiZ NICK Kilroy')
   end
 
   def test_parse_topic_change
     message = ':Wiz TOPIC #test :New topic'
-    assert_equal [:topic, 'Wiz', '#test', 'New topic'], @protocol.parse(message)
+    assert_equal [:topic, 'Wiz', '#test', 'New topic'], @parser.parse(message)
   end
 
   def test_parse_channel_invite
     message = ':Angel INVITE Wiz #Dust'
-    assert_equal [:invite, 'Angel', 'Wiz', '#Dust'], @protocol.parse(message)
+    assert_equal [:invite, 'Angel', 'Wiz', '#Dust'], @parser.parse(message)
   end
 
   def test_parse_channel_kick
     message = ':WiZ KICK #Finnish John'
-    assert_equal [:kick, 'WiZ', '#Finnish', 'John'], @protocol.parse(message)
+    assert_equal [:kick, 'WiZ', '#Finnish', 'John'], @parser.parse(message)
   end
 
   def test_parse_private_message
     message = ':Angel PRIVMSG Wiz :You there?'
-    assert_equal [:privmsg, 'Angel', 'Wiz', 'You there?'], @protocol.parse(message)
+    assert_equal [:privmsg, 'Angel', 'Wiz', 'You there?'], @parser.parse(message)
   end
 
   def test_parse_channel_server_quit
     message = ':Trillian SQUIT localhost :Bye!'
-    assert_equal [:squit, 'Trillian', 'localhost', 'Bye!'], @protocol.parse(message)
+    assert_equal [:squit, 'Trillian', 'localhost', 'Bye!'], @parser.parse(message)
   end
 end
