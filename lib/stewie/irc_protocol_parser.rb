fafe7e3f5a60753df8e6bd8bc3b1b3bfe553365b
Cainã Costa <cainan.costa@gmail.com>
2011-10-06 22:50:04 -0300

    Add parenthesis to remove warnings.

--- lib/stewie/irc_protocol_parser.rb
+++ lib/stewie/irc_protocol_parser.rb
@@ -3,13 +3,13 @@ module Stewie
     def parse(message)
       case message
       when /NICK/
-        parsed = message.match /:(?<old_nick>.*) NICK (?<new_nick>.*)/
+        parsed = message.match(/:(?<old_nick>.*) NICK (?<new_nick>.*)/)
         [:nick, parsed[:old_nick], parsed[:new_nick]]
       when /SQUIT/
-        parsed = message.match /:(?<nick>.*) SQUIT (?<host>.*) :(?<message>.*)/
+        parsed = message.match(/:(?<nick>.*) SQUIT (?<host>.*) :(?<message>.*)/)
         [:squit, parsed[:nick], parsed[:host], parsed[:message]]
       when /USER/
-        parsed = message.match /:(?<nick>.*) USER (?<username>.*) (?<hostname>.*) (?<servername>.*) :(?<realname>.*)/
+        parsed = message.match(/:(?<nick>.*) USER (?<username>.*) (?<hostname>.*) (?<servername>.*) :(?<realname>.*)/)
         [:user, parsed[:nick], parsed[:username], parsed[:hostname], parsed[:servername], parsed[:realname]]
       end
     end
